1c0d2b7a4f5d5685a0ebbe71ccee1f3bf14eb7f3
Rudolf Jacobs <rudolf@a-i-solutions.co.za>
2025-12-04 14:20:22 +0200

    chore: remove stub reports

--- app/Account/urls.py
+++ app/Account/urls.py
@@ -1,23 +1,7 @@
 from django.urls import path
 
-from .views_reports import (
-    ComplianceView,
-    CostPerformanceView,
-    DashboardView,
-    SchedulePerformanceView,
-)
 
 app_name = "account"
 
-reports_urls = [
-    path("dashboard/", DashboardView.as_view(), name="dashboard"),
-    path("cost-performance/", CostPerformanceView.as_view(), name="cost_performance"),
-    path(
-        "schedule-performance/",
-        SchedulePerformanceView.as_view(),
-        name="schedule_performance",
-    ),
-    path("compliance/", ComplianceView.as_view(), name="compliance"),
-]
 
-urlpatterns = reports_urls
+urlpatterns = []
